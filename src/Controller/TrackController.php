c7083e91dc56956266b061c330b7af067d02ecfa
lea905 <leabarnezet@gmail.com>
2025-10-07 14:50:58 +0200

    [ADD] index and show track

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -1,20 +1,68 @@
 <?php
 namespace App\Controller;
 
+use App\Service\AuthSpotifyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 
 #[Route('/track')]
 class TrackController extends AbstractController
 {
-    #[Route('/index/{max}', name: 'app_lucky_number')]
-    public function number(int $max): Response
+
+    private string $token;
+    private $httpClient;
+
+    public function __construct(private readonly AuthSpotifyService $authSpotifyService, HttpClientInterface $httpClient){
+        $this->token = $this->authSpotifyService->auth();
+        $this->httpClient = $httpClient;
+    }
+
+    #[Route('/', name: 'app_track_index')]
+    public function index(Request $request): Response
     {
-        $number = random_int(0, $max);
+        $query = $request->query->get('q', 'booba');
+        if (empty($query)) {
+            $query = 'booba';
+        }
+
+        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/search?query=' . urlencode($query) . '&type=track&local=fr-FR', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+            ]
+        ]);
+
+        $data = json_decode($response->getContent(), true);
+        $tracks = $data['tracks']['items'];
+
+        dump($tracks);
 
         return $this->render('track/index.html.twig', [
-            'number' => $number
+            'tracks' => $tracks,
+            'query' => $query,
         ]);
     }
+
+
+
+    #[Route('/{id}', name: 'app_track_show')]
+    public function show(string $id): Response
+    {
+        $response = $this->httpClient->request('GET', 'https://api.spotify.com/v1/tracks/' . $id, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+            ]
+        ]);
+
+        $track = json_decode($response->getContent(), true);
+
+        return $this->render('track/show.html.twig', [
+            'track' => $track,
+        ]);
+
+    }
+
 }
--- src/Service/AuthSpotifyService.php
+++ src/Service/AuthSpotifyService.php
@@ -1,2 +1,54 @@
 <?php
 
+namespace App\Service;
+
+use Doctrine\Tests\ORM\Functional\Ticket\Authorization;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class AuthSpotifyService
+{
+    private string $clientId;
+    private string $clientSecret;
+    private HttpClientInterface $httpClient;
+    private RequestStack $requestStack;
+
+    public function __construct(HttpClientInterface $httpClient, RequestStack $requestStack, string $clientId, string $clientSecret)
+    {
+        $this->httpClient = $httpClient;
+        $this->requestStack = $requestStack;
+        $this->clientId = $clientId;
+        $this->clientSecret = $clientSecret;
+    }
+
+    public function auth(): string
+    {
+        $newTokenNeeded = false;
+        $session = $this->requestStack->getSession();
+        if (!$session->has('token') || $session->get('expire') <= time()) {
+            $newTokenNeeded = true;
+        }
+
+        if ($newTokenNeeded) {
+            $response = $this->httpClient->request('POST', 'https://accounts.spotify.com/api/token', [
+                'headers' => [
+                    'Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret),
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                ],
+                'body' => [
+                    'grant_type' => 'client_credentials',
+                ],
+            ]);
+
+            $data = $response->toArray();
+
+
+            $session->set('token', $data['access_token']);
+            $session->set('expire', time() + 3600);
+            return $data['access_token'];
+        } else {
+            return $session->get('token');
+        }
+    }
+
+}
